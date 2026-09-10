49fa366422e28d4209c596fbe684304e90974794
Qoshima <92686470+Qoshima@users.noreply.github.com>
2026-09-10 23:13:36 +0500

    Update WarriorBuilder.java

--- src/gamecharacter/concretebuilder/WarriorBuilder.java
+++ src/gamecharacter/concretebuilder/WarriorBuilder.java
@@ -1,4 +1,62 @@
 package gamecharacter.concretebuilder;
+import gamecharacter.CharacterBuilder;
+import gamecharacter.GameCharacter;
 
-public class WarriorBuilder {
+public class WarriorBuilder implements CharacterBuilder{
+    private GameCharacter character = new GameCharacter();
+    public WarriorBuilder(){
+        character.setCharacterClass("Warrior");
+    }
+    @Override
+    public CharacterBuilder setName(String name) {
+        character.setName(name);
+        return this;
+    }
+
+    @Override
+    public CharacterBuilder setWeapon(String weapon) {
+        character.setWeapon(weapon);
+        return this;
+    }
+
+    @Override
+    public CharacterBuilder setArmor(String armor) {
+        character.setArmor(armor);
+        return this;
+    }
+
+    @Override
+    public CharacterBuilder setHealth(int health) {
+        character.setHealth(health);
+        return this;
+    }
+
+    @Override
+    public CharacterBuilder setMana(int mana) {
+        character.setMana(mana);
+        return this;
+    }
+
+    @Override
+    public CharacterBuilder setStrength(int strength) {
+        character.setStrength(strength);
+        return this;
+    }
+
+    @Override
+    public CharacterBuilder setAgility(int agility) {
+        character.setAgility(agility);
+        return this;
+    }
+
+    @Override
+    public CharacterBuilder setDefense(int defense) {
+        character.setDefense(defense);
+        return this;
+    }
+
+    @Override
+    public GameCharacter build() {
+        return character;
+    }
 }
