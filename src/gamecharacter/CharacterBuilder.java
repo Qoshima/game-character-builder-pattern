af9ccef933706ed08c12699f2b863c1d1254aba3
Qoshima <92686470+Qoshima@users.noreply.github.com>
2026-09-10 22:15:52 +0500

    CharacterBuilder interface

--- src/gamecharacter/CharacterBuilder.java
+++ src/gamecharacter/CharacterBuilder.java
@@ -1,4 +1,21 @@
 package gamecharacter;
 
 public class CharacterBuilder {
+    CharacterBuilder setName(String name);
+
+    CharacterBuilder setWeapon(String weapon);
+
+    CharacterBuilder setArmor(String armor);
+
+    CharacterBuilder setHealth(int health);
+
+    CharacterBuilder setMana(int mana);
+
+    CharacterBuilder setStrength(int strength);
+
+    CharacterBuilder setAgility(int agility);
+
+    CharacterBuilder setDefense(int defense);
+
+    GameCharacter build();
 }
