c1e665bc815d17410e8db42fd4503bbba1a8fa64
Qoshima <92686470+Qoshima@users.noreply.github.com>
2026-09-11 23:03:02 +0500

    Update CharacterDirector.java
    Director work with all concretebuilders same

--- src/gamecharacter/CharacterDirector.java
+++ src/gamecharacter/CharacterDirector.java
@@ -1,4 +1,19 @@
 package gamecharacter;
 
 public class CharacterDirector {
+    private CharacterBuilder builder;
+
+    public CharacterDirector(CharacterBuilder builder){
+        this.builder = builder;
+    }
+
+    public void setBuilder(CharacterBuilder builder){
+        this.builder = builder;
+    }
+
+    public GameCharacter constructCharacter(String name){
+        return builder
+                .setName(name)
+                .build();
+    }
 }
