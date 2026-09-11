2bac9b925e8053f6a25c9ab445ffb479c49b2765
Qoshima <92686470+Qoshima@users.noreply.github.com>
2026-09-11 22:29:58 +0500

    Tank Builder created
    Added constructor, default values

--- src/gamecharacter/concretebuilder/TankBuilder.java
+++ src/gamecharacter/concretebuilder/TankBuilder.java
@@ -1,4 +1,70 @@
 package gamecharacter.concretebuilder;
+import gamecharacter.CharacterBuilder;
+import gamecharacter.GameCharacter;
 
-public class TankBuilder {
+public class TankBuilder implements CharacterBuilder{
+    private final GameCharacter character = new GameCharacter();
+    public TankBuilder(){
+        character.setCharacterClass("Tank");
+        character.setWeapon("Shield");
+        character.setArmor("Plate armor");
+        character.setHealth(200);
+        character.setMana(40);
+        character.setStrength(90);
+        character.setAgility(40);
+        character.setDefense(95);
+    }
+
+    @Override
+    public CharacterBuilder setName(String name) {
+        character.setName(name);
+        return this;
+    }
+
+    @Override
+    public CharacterBuilder setWeapon(String weapon) {
+        character.setWeapon(weapon);
+        return this;
+    }
+
+    @Override
+    public CharacterBuilder setArmor(String armor) {
+        character.setArmor(armor);
+        return this;
+    }
+
+    @Override
+    public CharacterBuilder setHealth(int health) {
+        character.setHealth(health);
+        return this;
+    }
+
+    @Override
+    public CharacterBuilder setMana(int mana) {
+        character.setMana(mana);
+        return this;
+    }
+
+    @Override
+    public CharacterBuilder setStrength(int strength) {
+        character.setStrength(strength);
+        return this;
+    }
+
+    @Override
+    public CharacterBuilder setAgility(int agility) {
+        character.setAgility(agility);
+        return this;
+    }
+
+    @Override
+    public CharacterBuilder setDefense(int defense) {
+        character.setDefense(defense);
+        return this;
+    }
+
+    @Override
+    public GameCharacter build() {
+        return character;
+    }
 }
