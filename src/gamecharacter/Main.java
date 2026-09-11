f0352f1f4c189094a60bffc6d69f45f50604edf3
Qoshima <92686470+Qoshima@users.noreply.github.com>
2026-09-12 00:20:29 +0500

    Client demonstration

--- src/gamecharacter/Main.java
+++ src/gamecharacter/Main.java
@@ -1,4 +1,23 @@
 package gamecharacter;
+import gamecharacter.concretebuilder.*;
 
 public class Main {
+    public static void main(String[] args){
+        CharacterDirector director = new CharacterDirector(new WarriorBuilder());
+
+        GameCharacter warrior = director.constructCharacter("Gideon");
+        System.out.println(warrior);
+
+        director.setBuilder(new MageBuilder());
+        GameCharacter mage = director.constructCharacter("Merlin");
+        System.out.println(mage);
+
+        director.setBuilder(new ArcherBuilder());
+        GameCharacter archer = director.constructCharacter("Robin");
+        System.out.println(archer);
+
+        director.setBuilder(new TankBuilder());
+        GameCharacter tank = director.constructCharacter("Arthas");
+        System.out.println(tank);
+    }
 }
